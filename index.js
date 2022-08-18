2ec335b92e269d92c3a658118f57a181cd3f9681
Felix <barosiofelix@gmail.com>
2022-08-18 10:51:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const superbowlWin = (record) => {
+    const result = record.find((record) => {
+        return record.result === 'W'
+    })
+    if(result){
+        return result.year
+    }else{
+        return undefined
+    }
+}
